b8e926d432b0853d778f1ba34d8aa40181e90586
Damir Sadikov <sadikov_d@master.nngu>
2025-11-09 20:14:18 +0300

    run graphs

--- graphs/run_graphs_py.sh
+++ graphs/run_graphs_py.sh
@@ -5,6 +5,6 @@
 
 echo -n "START RUNNING graphs.py " && date
 
-python3 ./graphs.py ../out-XXXXXX-run_main2_1.out ../out-XXXXXX-run_main2_2.out ../out-XXXXXX-run_main2_3.out ../out-XXXXXX-run_main2_4.out
+python3 ./graphs.py ../out-XXXXXX-run_main2_1.out ../out-XXXXXX-run_main2_2.out ../out-XXXXXX-run_main2_4.out ../out-XXXXXX-run_main2_8.out
 
 echo -n "END RUNNING graphs.py " && date
